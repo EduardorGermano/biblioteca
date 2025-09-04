209c9376b08e1eb37b64309d361952e1da5574f3
Eduardo Germano <eduardor.germano@hotmail.com>
2025-09-03 21:51:53 -0300

    Aplicação do context na tela inicial

--- paginas/views.py
+++ paginas/views.py
@@ -1,6 +1,18 @@
 from django.views.generic import TemplateView
+from django.utils import timezone
+from livros.models import Livro,Emprestimo
 
 # Create your views here.
 
+
 class PaginaView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context['livros_disponiveis'] = Livro.objects.filter(disponivel=True).count()
+        context['emprestimos_ativos'] = Emprestimo.objects.filter(status='ativo').count()
+        context['emprestimos_atrasados'] = Emprestimo.objects.filter(status='atrasado').count()
+
+        return context
